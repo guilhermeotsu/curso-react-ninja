774523cc3b763dab0c02fcaeed6207dd89a491b2
Guilherme <guiotsu@gmail.com>
2020-07-09 14:54:41 -0300

    Formulario: checkbox e radio

---
+++ modulo1/react-formularios/src/app.js
@@ -0,0 +1,53 @@
+'use strict'
+
+import React from 'react'
+
+// input se refere a um componente gerado pelo react, e nao a tag input do html, passando props
+// para permitir que o input seja modificado é necessário o metodo onChange
+// essa forma de modificar o input é chamada de controled components (mais recomendado), mas da para fazer de outras formas
+
+// Como identificar componentes controled
+// input --> Propriedade value
+// checkbox --> Propriedade checked
+// radio --> Propriedade value (sao controlados pelo nome)
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      checked: false,
+      value: 'init value'
+    }
+  }
+  render () {
+    return (
+      <div>
+        <form>
+          <input
+            type='text'
+            value={this.state.value}
+            onChange={e => {
+              this.setState({
+                value: e.target.value
+              })
+            }}
+          />
+
+          <label>
+            <input
+              type='checkbox'
+              checked={this.state.checked}
+              onChange={() => {
+                this.setState({ checked: !this.state.checked })
+              }}
+            />
+            checkbox
+          </label>
+
+          <input type='radio' name='rd' value='1' defaultChecked /> Radio 1
+          <input type='radio' name='rd' value='2' /> Radio 2
+        </form>
+      </div>
+    )
+  }
+}
+export default App
