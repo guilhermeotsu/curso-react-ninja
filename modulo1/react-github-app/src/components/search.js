1d3e42c1b7cb28b4dc72515794aa5d05c034b8ed
Guilherme <guiotsu@gmail.com>
2020-07-11 16:13:24 -0300

    Fazendo o request para a API do Github

--- modulo1/react-github-app/src/components/search.js
+++ modulo1/react-github-app/src/components/search.js
@@ -1,10 +1,25 @@
 'use strict'
 
 import React from 'react'
+import ajax from '@fdaciuk/ajax'
 
 const Search = () => (
   <div className='search'>
-    <input type='search' placeholder='Digite o nome do usuário' />
+    <input
+      type='search'
+      placeholder='Digite o nome do usuário'
+      onKeyUp={(e) => {
+        const value = e.target.value
+        const key = e.which || e.keyCode
+        const ENTERKEY = 13
+        if (key === ENTERKEY) {
+          ajax().get(`https://api.github.com/users/${value}`)
+            .then((result) => {
+              console.log(result)
+            })
+        }
+      }}
+    />
   </div>
 )
 
