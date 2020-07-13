b873f3578ed22f86d925c623125defd8dfeca5f2
Guilherme <guiotsu@gmail.com>
2020-07-13 17:14:16 -0300

    TDD - every

---
+++ modulo2/react-jest-in-practice/src/every/every.test.js
@@ -0,0 +1,32 @@
+'use strict'
+
+import { expect, util } from 'chai'
+import every from './every'
+
+it('every deve ser uma função', () => {
+  expect(every).to.be.a('function')
+})
+
+it('every([1, 2], (item) => item > 0) deve retornar true', () => {
+  expect(every([1, 2], (item) => item > 0)).to.be.true
+})
+
+it('every([1, 2], (item) => item < 0) deve retornar false', () => {
+  expect(every([1, 2], (item) => item < 0)).to.be.false
+})
+
+it('every([0, 1]) deve retornar false', () => {
+  expect(every([0, 1])).to.be.false
+})
+
+it('every(["", 1, 2, 3]) deve retornar false', () => {
+  expect(every(["", 1, 2, 3])).to.be.false
+})
+
+it('every([10, 20, 30], (item, index) => index > 1) deve retornar false', () => {
+  expect(every([10, 20, 30], (item, index) => index > 1)).to.be.false
+})
+
+it('every([1, 2, 3], (item, index, arr) => arr.length >= index) deve retornar true', () => {
+  expect(every([1, 2, 3], (item, index, arr) => arr.length >= index)).to.be.true 
+})
