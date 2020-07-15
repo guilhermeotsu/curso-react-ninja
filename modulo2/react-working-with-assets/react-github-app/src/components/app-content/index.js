19085181f01359c5695c5fea2208a3d758b30265
Guilherme <guiotsu@gmail.com>
2020-07-15 14:41:59 -0300

    Adicionando Alias no Webpack

--- modulo2/react-working-with-assets/react-github-app/src/components/app-content/index.js
+++ modulo2/react-working-with-assets/react-github-app/src/components/app-content/index.js
@@ -1,10 +1,10 @@
 'use strict'
 
 import React, { PropTypes } from 'react'
-import Search from './../search'
-import UserInfo from './../user-info'
-import Actions from './../actions'
-import Repos from './../repos'
+import Search from 'components/search'
+import UserInfo from 'components/user-info'
+import Actions from 'components/actions'
+import Repos from 'components/repos'
 import style from './app-content.css'
 
 // !! dentro do userinfo esta transformando a prop em booleano
