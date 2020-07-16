d80b824277892d5cbb47328163a77a13ae5881a4
Guilherme <guiotsu@gmail.com>
2020-07-16 15:24:24 -0300

    Paginação - parte 2

--- modulo2/react-github-app-with-pagination/src/utils/pagination/index.js
+++ modulo2/react-github-app-with-pagination/src/utils/pagination/index.js
@@ -1,9 +1,30 @@
 'use strict'
 
+
+const centerRule = ({ total, activePage }) => {
+  if (activePage - 1 <= 0) {
+   return 1 
+  }
+  return activePage - 1
+}
+
 const pagination = ({ total, activePage }) => {
-  // Cria um Array do tamanho do total e mapea cada propriedade passando index + 1
-  // return Array.apply(null, { length: total }).map((_, index) => ++index) --> ES5
-  return Array.from({ length:total }, (_, i) => ++i) // --> ES6
+  if(total <= 5) {
+    // Cria um Array do tamanho do total e mapeia cada propriedade passando index + 1
+    // return Array.apply(null, { length: total }).map((_, index) => ++index) --> ES5
+    return Array.from({ length: total }, (_, i) => i + 1) // --> ES6
+  }
+
+  const visiblePages = 3
+  let pages = [
+    1,
+    ...Array.from({length: visiblePages }, (_, i) => i + centerRule({total, activePage})),
+    total
+  ]
+
+  pages = pages.filter((page, index, array) => array.indexOf(page) === index)
+
+  return pages
 }
 
 export default pagination
--- modulo2/react-github-app-with-pagination/src/utils/pagination/pagination.test.js
+++ modulo2/react-github-app-with-pagination/src/utils/pagination/pagination.test.js
@@ -14,3 +14,20 @@ it('pagination({ total: 1, activePage: 1}) deve retornar [1]', () => {
 it('pagination({ total: 2, activePage: 2 }) deve retornar [1, 2]', () => {
   expect(pagination({ total: 2, activePage: 2 })).to.be.deep.equal([1, 2])
 })
+
+// Independente da pagina ativa vai ser o array inteiro :
+it('pagination({ total: 5, activePage: 1 }) deve retornar [1, 2, 3, 4, 5]', () => {
+  expect(pagination({ total: 5, activePage: 1 })).to.be.deep.equal([1, 2, 3, 4, 5])
+})
+
+it('pagination({ total: 6, activePage: 1 }) deve retornar [1, 2, 3, "...", 6]', () => {
+  expect(pagination({ total: 6, activePage: 1 })).to.be.deep.equal([1, 2, 3, "...", 6])
+})
+
+it('pagination({ total: 6, activePage: 2 }) deve retornar [1, 2, 3, "...", 6]', () => {
+  expect(pagination({ total: 6, activePage: 2 })).to.be.deep.equal([1, 2, 3, "...", 6])
+})
+
+it('pagination({ total: 5, activePage: 3 }) deve retornar [1, 2, 3, 4, 5, 6]', () => {
+  expect(pagination({ total: 6, activePage: 3 })).to.be.deep.equal([1, 2, 3, 4, 5, 6])
+})
