0dc36f48eecb780f09f5a0cbb6cadbed2a2061a3
Guilherme <guiotsu@gmail.com>
2020-07-20 17:56:10 -0300

    Implementaçao do componente de paginaçao

--- modulo2/react-github-app-with-pagination/src/utils/pagination/index.js
+++ modulo2/react-github-app-with-pagination/src/utils/pagination/index.js
@@ -1,28 +1,80 @@
 'use strict'
 
+const centerRule = ({ total, activePage }) => (
+  activePage - 1 <= 0
+    ? 1
+    : activePage === total
+      ? activePage - 2
+      : activePage - 1
+)
 
-const centerRule = ({ total, activePage }) => {
-  if (activePage - 1 <= 0) {
-   return 1 
+const isNumber = (value) => typeof value === 'number'
+
+const pagination = ({ total = 1, activePage = 1 } = {}) => {
+  if (!isNumber(total)) {
+    throw new TypeError('total should be a number')
   }
-  return activePage - 1
-}
 
-const pagination = ({ total, activePage }) => {
-  if(total <= 5) {
-    // Cria um Array do tamanho do total e mapeia cada propriedade passando index + 1
-    // return Array.apply(null, { length: total }).map((_, index) => ++index) --> ES5
-    return Array.from({ length: total }, (_, i) => i + 1) // --> ES6
+  if (!isNumber(activePage)) {
+    throw new TypeError('activePage should be a number')
+  }
+
+  if (total <= 5) {
+    return Array.from({ length: total }, (_, i) => i + 1)
   }
 
   const visiblePages = 3
   let pages = [
     1,
-    ...Array.from({length: visiblePages }, (_, i) => i + centerRule({total, activePage})),
+    ...Array.from({ length: visiblePages }, (_, i) => i + centerRule({ total, activePage })),
     total
   ]
 
   pages = pages.filter((page, index, array) => array.indexOf(page) === index)
+
+  let firstPage = pages[0]
+  let secondPage = pages[1]
+
+  if (secondPage === (firstPage + 2)) {
+    pages = [
+      firstPage,
+      firstPage + 1,
+      ...pages.slice(1)
+    ]
+  }
+
+  let penultimatePage = pages[pages.length - 2]
+  let lastPage = pages[pages.length - 1]
+
+  if (penultimatePage === (lastPage - 2)) {
+    pages = [
+      ...pages.slice(0, -1),
+      lastPage - 1,
+      lastPage
+    ]
+  }
+
+  firstPage = pages[0]
+  secondPage = pages[1]
+
+  if (secondPage > (firstPage + 2)) {
+    pages = [
+      firstPage,
+      '...',
+      ...pages.slice(1)
+    ]
+  }
+
+  penultimatePage = pages[pages.length - 2]
+  lastPage = pages[pages.length - 1]
+
+  if (penultimatePage < (lastPage - 2)) {
+    pages = [
+      ...pages.slice(0, -1),
+      '...',
+      lastPage
+    ]
+  }
 
   return pages
 }
