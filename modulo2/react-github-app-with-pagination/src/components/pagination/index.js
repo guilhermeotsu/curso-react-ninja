dd9efc26e0b11c14c06bf35836bdf1ba7aed8be6
Guilherme <guiotsu@gmail.com>
2020-07-22 12:04:53 -0300

    Criando storybook das pagination

--- modulo2/react-github-app-with-pagination/src/components/pagination/index.js
+++ modulo2/react-github-app-with-pagination/src/components/pagination/index.js
@@ -1,16 +1,29 @@
 'use strict'
 
-import React from 'react'
+import React, { PropTypes } from 'react'
 import pagination from 'utils/pagination'
+import Page from './page'
+import style from './pagination.css'
 
-const Pagination = ({ total, activePage }) => (
-  <ul>
+const Pagination = ({ total, activePage, pageLink, onClick }) => (
+  <ul className={style.pagination}>
     {pagination({ total, activePage }).map((page, index) => (
-      <li key={index}>
-        <a href="#" >{page}</a>
+      <li key={index} className={activePage === page ? style.activePage : null } >
+        <Page page={page} pageLink={pageLink.replace('%page%', page)} onClick={onClick} />
       </li>
     ))}
   </ul>
 )
+
+Pagination.defaultProps = {
+  pageLink: ''
+}
+
+Pagination.propTypes = {
+  total: PropTypes.number,
+  activePage: PropTypes.number,
+  pageLink: PropTypes.string,
+  onClick: PropTypes.func
+}
 
 export default Pagination
--- modulo2/react-github-app-with-pagination/src/components/pagination/pagination.story.js
+++ modulo2/react-github-app-with-pagination/src/components/pagination/pagination.story.js
@@ -4,7 +4,16 @@ import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import Pagination from './index'
 
-storiesOf('<Pagination />', module)
+storiesOf('Pagination', module)
   .add('without props', () => (
-    <Pagination total={10} />
+    <Pagination />
+  ))
+  .add('with total and active page', () => (
+    <Pagination total={15} activePage={3} />
+  ))
+  .add('with pageLink', () => (
+    <Pagination total={15} activePage={5} pageLink='https://mypage.com/page/%page%' />
+  ))
+  .add('with callback function', () => (
+    <Pagination total={15} activePage={10} pageLink='https://mypage.com/page/%page%' onClick={(page) => window.alert(page)} />
   ))
