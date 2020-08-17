2e894d10ef00e753174bd2005fbd7283acdda797
Guilherme Otsu <guiotsu@gmail.com>
2020-08-17 11:28:01 -0300

    Modulo 3 - Primeiro contato com Redux

---
+++ modulo3/01-hello-redux/main.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1
+    case 'DECREMENT': return state - 1
+    default: return state
+  }
+}
+
+console.assert(
+  counter(0, { type: 'INCREMENT' }) === 1,
+  "counter(0, { type: 'INCREMENT' }) === 1" 
+)
+
+console.assert(
+  counter(1, { type: 'INCREMENT' }) === 2,
+  "counter(1, { type: 'INCREMENT' }) === 2,"
+)
+
+console.assert(
+  counter(2, { type: 'DECREMENT' }) === 1,
+  "counter(2, { type: 'DECREMENT' }) === 1,"
+)
+
+console.assert(
+  counter(1, { type: 'DECREMENT' }) === 0,
+  "counter(1, { type: 'DECREMENT' }) === 0,"
+)
+
+console.assert(
+  counter(3, { type: 'SOMETHING' }) === 3,
+  "counter(3, { type: 'SOMETHING' }) === 3"
+)
+
+console.assert(
+  counter(undefined, {}) === 0,
+  "counter(undefined, {}) === 0"
+)
